4653b90bb09f984d396a75581c5f8eb152000097
umar <umar.dev500@gmail.com>
2023-03-04 19:16:01 +0700

    add helper

---
+++ helper/get_time.go
@@ -0,0 +1,23 @@
+package helper
+
+import (
+	"order/variable"
+	"time"
+)
+
+func GetTime(timeType *string) (res int64) {
+	t := time.Now().UTC()
+
+	switch timeType {
+	case &variable.UnixNano:
+		res = t.UnixNano()
+	case &variable.UnixMili:
+		res = t.UnixMilli()
+	case &variable.UnixMicro:
+		res = t.UnixMicro()
+	default:
+		res = t.Unix()
+	}
+
+	return
+}
